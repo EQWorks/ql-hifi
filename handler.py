14949df7a4a6bf8188907ed3d09a3baf40caece8
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2022-06-10 17:12:33 -0400

    proxy - conditionally init duckdb with s3 connectivity if query mentions s3

--- handler.py
+++ handler.py
@@ -4,6 +4,11 @@ from modules import duck
 
 
 def query(event, context):
+    q = event.get('query', event.get('q'))
+
+    if 's3://' in q:
+        duck.init()  # init the in-memory database with S3 connectivity
+
     data = duck.query(event.get('query', event.get('q')))
     return {
         'statusCode': 200,
