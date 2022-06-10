61e0fcb928964c6777c3092eec8c8cc379767413
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2022-06-10 17:41:14 -0400

    devops - revert to use explicit but bypassing reserved AWS key/secret env vars

--- modules/duck.py
+++ modules/duck.py
@@ -18,8 +18,8 @@ def init():
                 LOAD httpfs;
 
                 SET s3_region='us-east-1';
-                SET s3_access_key_id='{os.getenv("AWS_ACCESS_KEY_ID")}';
-                SET s3_secret_access_key='{os.getenv("AWS_SECRET_ACCESS_KEY")}';
+                SET s3_access_key_id='{os.getenv("AWS_KEY")}';
+                SET s3_secret_access_key='{os.getenv("AWS_SECRET")}';
             """
         )
 
