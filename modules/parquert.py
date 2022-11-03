d1f9c2cf24c545ab7bc783b05172f30099d6c93d
amy83762100 <76548841+amy83762100@users.noreply.github.com>
2022-11-03 12:23:59 -0400

    parquert - use pyarrow instead of pandas

--- modules/parquert.py
+++ modules/parquert.py
@@ -2,7 +2,8 @@ import os
 import json
 import gzip
 import boto3
-import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
 
 
 def to_parquert(bucket, key) -> None:
@@ -14,9 +15,10 @@ def to_parquert(bucket, key) -> None:
     )
     filename = 'tmp/results.parquet'
     obj = s3.get_object(Bucket=bucket, Key=key)
-    with gzip.open(obj['Body'], 'rt') as gf:
-        df = pd.read_json(gf)
-        df.to_parquet(filename)
+    with gzip.open(obj['Body'], 'rt', encoding='UTF-8') as gf:
+        data = json.loads(gf.read())
+        table = pa.Table.from_pylist(data)
+        pq.write_table(table, filename)
 
     with open(filename, 'rb') as data:
         s3.upload_fileobj(data, bucket, f'{key}.parquet')
