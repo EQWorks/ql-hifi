6347deedabbd17b148b6b10703961bdea74aa8fb
Pei-Yi Lin <amy83762100@gmail.com>
2022-11-16 10:11:44 -0500

    query - upload result to s3

---
+++ modules/s3.py
@@ -0,0 +1,14 @@
+import boto3
+
+
+def put_to_s3(bucket, key, body, ttl=600) -> str:
+    s3 = boto3.client('s3')
+    s3.put_object(Bucket=bucket, Key=key, Body=body, ContentType='application/json')
+    return s3.generate_presigned_url(
+        'get_object',
+        Params={
+            'Bucket': bucket,
+            'Key': key,
+        },
+        ExpiresIn=ttl,
+    )
