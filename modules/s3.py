f408f6eb2ab120f4c763534d15e28b62b3839b42
Pei-Yi Lin <amy83762100@gmail.com>
2022-11-28 10:29:12 -0500

    s3/access - add iam permissions to allow function to access s3 bucket

--- modules/s3.py
+++ modules/s3.py
@@ -3,12 +3,7 @@ import boto3
 
 
 def put_to_s3(bucket, key, body, ttl=600) -> str:
-    s3 = boto3.client(
-        service_name='s3',
-        aws_access_key_id=os.getenv('AWS_KEY'),
-        aws_secret_access_key=os.getenv('AWS_SECRET'),
-        region_name='us-east-1',
-    )
+    s3 = boto3.client('s3')
     s3.put_object(Bucket=bucket, Key=key, Body=body, ContentType='application/json')
     return s3.generate_presigned_url(
         'get_object',
